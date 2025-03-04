aaa1f9564d57568c2c5b755de488c846b3831ff4
realsanjeev <realsanjeev2@gmail.com>
2025-03-04 19:57:32 +0545

    Advance the turing Machine animation with table

--- src/plug.c
+++ src/plug.c
@@ -8,11 +8,6 @@
 #define ARENA_IMPLEMENTATION
 #include "arena.h"
 
-#define FONT_SIZE 52
-#define CELL_WIDTH 150.0f
-#define CELL_HEIGHT 100.0f
-#define CELL_PAD (CELL_WIDTH*0.15f)
-
 #if 0
     #define CELL_COLOR ColorFromHSV(0, 0.0, 0.15)
     #define HEAD_COLOR ColorFromHSV(200, 0.8, 0.8)
@@ -23,6 +18,11 @@
     #define BACKGROUND_COLOR ColorFromHSV(120, 0.0, 1 - 0.95)
 #endif
 
+#define FONT_SIZE 52
+#define CELL_WIDTH 150.0f
+#define CELL_HEIGHT 100.0f
+#define CELL_PAD (CELL_WIDTH*0.15f)
+#define START_AT_CELL_INDEX 10
 #define HEAD_MOVING_DURATION 0.5f
 #define HEAD_WRITING_DURATION 0.2f
 #define INTRO_DURATION 1.0f
@@ -37,6 +37,25 @@ static inline float smoothstep(float t) {
     return 3*t*t - 2*t*t*t;
 }
 
+typedef enum {
+    DIR_LEFT = -1,
+    DIR_RIGHT = 1,
+} Direction;
+
+typedef struct {
+    const char *state;
+    const char *read;
+    const char *write;
+    Direction step;
+    const char *next;
+} Rule;
+
+typedef struct {
+    Rule *items;
+    size_t count;
+    size_t capacity;
+} Table;
+
 typedef struct {
     const char *symbol;
 } Cell;
@@ -47,18 +66,15 @@ typedef struct {
     size_t capacity;
 } Tape;
 
-typedef enum {
-    DIR_LEFT = -1,
-    DIR_RIGHT = 1,
-} Direction;
-
 typedef struct {
     int index;
+    const char *state;
 } Head;
 
 typedef enum {
     ACTION_MOVE,
     ACTION_WRITE,
+    ACTION_SWITCH,
     ACTION_INTRO,
     ACTION_OUTRO,
     ACTION_WAIT,
@@ -67,6 +83,7 @@ typedef enum {
 typedef union {
     Direction move;
     const char *write;
+    const char *sweetch;
     size_t intro; // index of the tape to settle on
     size_t jump;  // action to jump to
     float wait;
@@ -93,83 +110,101 @@ typedef struct {
     Tape tape;
     Arena tape_strings;
     Camera2D camera;
+    float scene_t;
 
     // Assets (reloads along with plugin, does not change throughout the animation)
     Script script;
+    Table table;
     Font font;
     Sound plant;
+
+    // New
+    float tape_y_offset; // State
 } Plug;
 
 static Plug *p = NULL;
 
-static void action_intro(size_t intro) {
-    Action action = {
-        .kind = ACTION_INTRO,
-        .as = { .intro = intro },
-    };
+static void action(Action_Kind kind, ...) {
+    Action action = { .kind = kind };
+    va_list args;
+    va_start(args, kind);
+    switch (kind) {
+        case ACTION_MOVE:   action.as.move = va_arg(args, Direction);       break;
+        case ACTION_WRITE:  action.as.write = va_arg(args, const char *);   break;
+        case ACTION_SWITCH: action.as.sweetch = va_arg(args, const char *); break;
+        case ACTION_INTRO:  action.as.intro = va_arg(args, size_t);         break;
+        case ACTION_OUTRO:                                                  break;
+        case ACTION_WAIT:   action.as.wait = (float)va_arg(args, double);   break;
+        default:
+            fprintf(stderr, "UNREACHABLE\n");
+            abort();
+    }
+    va_end(args);
     nob_da_append(&p->script, action);
 }
 
-static void action_move(Direction move) {
-    Action action = {
-        .kind = ACTION_MOVE,
-        .as = { .move = move },
+static void table(const char *state, const char *read, const char *write, Direction step, const char *next) {
+    Rule rule = {
+        .state = state,
+        .read = read,
+        .write = write,
+        .step = step,
+        .next = next,
     };
-    nob_da_append(&p->script, action);
-}
-
-static void action_write(const char *write) {
-    Action action = {
-        .kind = ACTION_WRITE,
-        .as = { .write = write },
-    };
-    nob_da_append(&p->script, action);
-}
-
-static void action_wait(float wait) {
-    Action action = {
-        .kind = ACTION_WAIT,
-        .as = { .wait = wait },
-    };
-    nob_da_append(&p->script, action);
-}
-
-static void action_outro(void) {
-    Action action = {
-        .kind = ACTION_OUTRO,
-    };
-    nob_da_append(&p->script, action);
+    nob_da_append(&p->table, rule);
 }
 
 static void load_assets(void) {
     p->font = LoadFontEx("./assets/fonts/iosevka-regular.ttf", FONT_SIZE, NULL, 0);
     p->plant = LoadSound("./assets/sounds/plant-bomb.wav");
 
-    action_intro(10);
-    action_wait(0.25f);
-    action_write("Foo");
-    action_move(DIR_RIGHT);
-    action_write("Bar");
-    action_move(DIR_LEFT);
-    action_write("0");
-    action_move(DIR_RIGHT);
-    action_write("0");
-    action_move(DIR_RIGHT);
-    action_write("67");
-    action_write("68");
-    action_write("69");
-    action_move(DIR_RIGHT);
-    action_write("418");
-    action_write("419");
-    action_write("420");
-    action_wait(1.0f);
-    action_outro();
+    // Table
+    {
+        table("Inc", "0", "1", DIR_RIGHT, "Halt");
+        table("Inc", "1", "0", DIR_LEFT,  "Inc");
+    }
+
+    // Script
+    {
+        action(ACTION_INTRO, START_AT_CELL_INDEX);
+        action(ACTION_WAIT, 0.25f);
+        action(ACTION_SWITCH, "Inc");
+        action(ACTION_WRITE, "0");
+        action(ACTION_MOVE, DIR_RIGHT);
+        action(ACTION_WRITE, "0");
+        action(ACTION_MOVE, DIR_RIGHT);
+        action(ACTION_WRITE, "0");
+        action(ACTION_MOVE, DIR_RIGHT);
+        action(ACTION_WRITE, "1");
+        action(ACTION_SWITCH, "Halt");
+        action(ACTION_WAIT, 1.0f);
+        action(ACTION_SWITCH, "");
+        action(ACTION_OUTRO);
+
+        action(ACTION_WAIT, 1.0f);
+
+        action(ACTION_INTRO, START_AT_CELL_INDEX);
+        action(ACTION_WAIT, 0.25f);
+        action(ACTION_SWITCH, "Dec");
+        action(ACTION_WRITE, "1");
+        action(ACTION_MOVE, DIR_RIGHT);
+        action(ACTION_WRITE, "1");
+        action(ACTION_MOVE, DIR_RIGHT);
+        action(ACTION_WRITE, "1");
+        action(ACTION_MOVE, DIR_RIGHT);
+        action(ACTION_WRITE, "0");
+        action(ACTION_SWITCH, "Halt");
+        action(ACTION_WAIT, 1.0f);
+        action(ACTION_SWITCH, "");
+        action(ACTION_OUTRO);
+    }
 }
 
 static void unload_assets(void) {
     UnloadFont(p->font);
     UnloadSound(p->plant);
     p->script.count = 0;
+    p->table.count = 0;
 }
 
 void plug_reset(void)
@@ -177,14 +212,21 @@ void plug_reset(void)
     arena_reset(&p->tape_strings);
     p->tape.count = 0;
     p->head.index = 0;
+    p->head.state = arena_strdup(&p->tape_strings, "");
     p->ip = 0;
-    p->t = 0.0;
+    p->t = 0.0f;
+    p->tape_y_offset = 0.0f;
 
-    char *default_symbol = arena_strdup(&p->tape_strings, "0");
+    char *zero = arena_strdup(&p->tape_strings, "0");
+    char *one = arena_strdup(&p->tape_strings, "1");
     for (size_t i = 0; i < TAPE_SIZE; ++i) {
-        Cell cell = {.symbol = default_symbol,};
+        Cell cell = {.symbol = zero,};
         nob_da_append(&p->tape, cell);
     }
+
+    p->tape.items[START_AT_CELL_INDEX + 0].symbol = one;
+    p->tape.items[START_AT_CELL_INDEX + 1].symbol = one;
+    p->tape.items[START_AT_CELL_INDEX + 2].symbol = one;
 }
 
 void plug_init(void) {
@@ -192,8 +234,9 @@ void plug_init(void) {
     assert(p != NULL);
     memset(p, 0, sizeof(*p));
     p->size = sizeof(*p);
-    plug_reset();
+
     load_assets();
+    plug_reset();
 
     TraceLog(LOG_INFO, "---------------------");
     TraceLog(LOG_INFO, "Initialized Plugin");
@@ -215,8 +258,8 @@ void plug_post_reload(void *state) {
     load_assets();
 }
 
-void text_in_cell(Rectangle rec, const char *from_text, const char *to_text, float t) {
-    Vector2 cell_size = {CELL_WIDTH, CELL_HEIGHT};
+static void text_in_rec(Rectangle rec, const char *from_text, const char *to_text, float t, Color color) {
+    Vector2 cell_size = { rec.width, rec.height };
 
     {
         float font_size = FONT_SIZE*(1 - t);
@@ -227,7 +270,7 @@ void text_in_cell(Rectangle rec, const char *from_text, const char *to_text, flo
         };
         position = Vector2Add(position, Vector2Scale(cell_size, 0.5));
         position = Vector2Subtract(position, Vector2Scale(text_size, 0.5));
-        DrawTextEx(p->font, from_text, position, font_size, 0, ColorAlpha(BACKGROUND_COLOR, 1 - t));
+        DrawTextEx(p->font, from_text, position, font_size, 0, ColorAlpha(color, 1 - t));
     }
 
     {
@@ -239,44 +282,71 @@ void text_in_cell(Rectangle rec, const char *from_text, const char *to_text, flo
         };
         position = Vector2Add(position, Vector2Scale(cell_size, 0.5));
         position = Vector2Subtract(position, Vector2Scale(text_size, 0.5));
-        DrawTextEx(p->font, to_text, position, font_size, 0, ColorAlpha(BACKGROUND_COLOR, t));
+        DrawTextEx(p->font, to_text, position, font_size, 0, ColorAlpha(color, t));
     }
 }
 
 void render_tape(float w, float h, float t) {
+    float cell_width = CELL_WIDTH;
+    float cell_height = CELL_HEIGHT;
+    float cell_pad = CELL_PAD;
+
     for (size_t i = 0; i < p->tape.count; ++i) {
         Rectangle rec = {
-            .x = i*(CELL_WIDTH + CELL_PAD) + w/2 - CELL_WIDTH/2 - t*(CELL_WIDTH + CELL_PAD),
-            .y = h/2 - CELL_HEIGHT/2,
-            .width = CELL_WIDTH,
-            .height = CELL_HEIGHT,
+            .x = i*(cell_width + cell_pad) + w/2 - cell_width/2 - Lerp(-20.0, t, p->scene_t)*(cell_width + cell_pad),
+            .y = h/2 - cell_height/2 - p->tape_y_offset,
+            .width = cell_width,
+            .height = cell_height,
         };
         DrawRectangleRec(rec, CELL_COLOR);
 
         if (
-            (size_t)p->head.index == i &&               // we are rendering the head
-            p->ip < p->script.count &&                  // there is a currently executing instruction
-            p->script.items[p->ip].kind == ACTION_WRITE // that instruction is ACTION_WRITE
+            // we are rendering the head
+            (size_t)p->head.index == i &&
+            // there is a currently executing instruction
+            p->ip < p->script.count &&
+            // the instruction is ACTION_WRITE
+            p->script.items[p->ip].kind == ACTION_WRITE
         ) {
-            text_in_cell(rec, p->tape.items[i].symbol, p->script.items[p->ip].as.write, p->t);
+            text_in_rec(rec, p->tape.items[i].symbol, p->script.items[p->ip].as.write, p->t, BACKGROUND_COLOR);
         } else {
-            text_in_cell(rec, p->tape.items[i].symbol, "", 0);
+            text_in_rec(rec, p->tape.items[i].symbol, "", 0, BACKGROUND_COLOR);
         }
     }
 }
 
-void render_head(float w, float h, float t) {
+void render_head(float w, float h, float state_t) {
     float head_thick = 20.0;
     Rectangle rec = {
-        .width = CELL_WIDTH + head_thick*3 + (1 - t)*head_thick*3,
-        .height = CELL_HEIGHT + head_thick*3 + (1 - t)*head_thick*3,
+        .width = CELL_WIDTH + head_thick*3 + (1 - p->scene_t)*head_thick*3,
+        .height = CELL_HEIGHT + head_thick*3 + (1 - p->scene_t)*head_thick*3,
     };
     rec.x = w/2 - rec.width/2;
-    rec.y = h/2 - rec.height/2;
-    DrawRectangleLinesEx(rec, head_thick, ColorAlpha(HEAD_COLOR, t));
+    rec.y = h/2 - rec.height/2 - p->tape_y_offset;
+    DrawRectangleLinesEx(rec, head_thick, ColorAlpha(HEAD_COLOR, p->scene_t));
+
+    const char *text = p->head.state;
+    float font_size = FONT_SIZE;
+    Rectangle text_rec = {
+        .x = rec.x,
+        .y = rec.y + rec.height,
+        .width = rec.width,
+        .height = font_size*1.5,
+    };
+    if (
+        // there is a currently executing instruction
+        p->ip < p->script.count &&
+        // the instruction is ACTION_SWITCH
+        p->script.items[p->ip].kind == ACTION_SWITCH
+    ) {
+        text_in_rec(text_rec, text, p->script.items[p->ip].as.sweetch, state_t, ColorAlpha(CELL_COLOR, p->scene_t));
+    } else {
+        text_in_rec(text_rec, text, "", 0.0, ColorAlpha(CELL_COLOR, p->scene_t));
+    }
 }
 
 void plug_update(float dt, float w, float h, bool _rendering) {
+
     (void) _rendering;
 
     ClearBackground(BACKGROUND_COLOR);
@@ -287,7 +357,7 @@ void plug_update(float dt, float w, float h, bool _rendering) {
             case ACTION_WAIT: {
                 p->t = (p->t*action.as.wait + dt)/action.as.wait;
                 render_tape(w, h, (float)p->head.index);
-                render_head(w, h, 1.0);
+                render_head(w, h, 0.0);
 
                 if (p->t >= 1.0f) {
                     p->ip += 1;
@@ -297,8 +367,9 @@ void plug_update(float dt, float w, float h, bool _rendering) {
 
             case ACTION_INTRO: {
                 p->t = (p->t*INTRO_DURATION + dt)/INTRO_DURATION;
-                render_tape(w, h, Lerp(-20.0, (float)action.as.intro, sinstep(p->t)));
-                render_head(w, h, sinstep(p->t));
+                p->scene_t = sinstep(p->t);
+                render_tape(w, h, (float)action.as.intro);
+                render_head(w, h, 0.0f);
 
                 if (p->t >= 1.0) {
                     p->head.index = action.as.intro;
@@ -309,8 +380,9 @@ void plug_update(float dt, float w, float h, bool _rendering) {
 
             case ACTION_OUTRO: {
                 p->t = (p->t*INTRO_DURATION + dt)/INTRO_DURATION;
-                render_tape(w, h, Lerp((float)p->head.index, -20.0, sinstep(p->t)));
-                render_head(w, h, sinstep(1.0f - p->t));
+                p->scene_t = sinstep(1.0f - p->t);
+                render_tape(w, h, (float)p->head.index);
+                render_head(w, h, 0.0f);
 
                 if (p->t >= 1.0) {
                     p->ip += 1;
@@ -325,10 +397,24 @@ void plug_update(float dt, float w, float h, bool _rendering) {
                 float to = (float)(p->head.index + action.as.move);
                 float t = Lerp(from, to, sinstep(p->t));
                 render_tape(w, h, t);
-                render_head(w, h, 1.0f);
+                render_head(w, h, 0.0f);
 
                 if (p->t >= 1.0) {
                     p->head.index += action.as.move;
+                    p->ip += 1;
+                    p->t = 0;
+                }
+            } break;
+
+            case ACTION_SWITCH: {
+                p->t = (p->t*HEAD_WRITING_DURATION + dt)/HEAD_WRITING_DURATION;
+
+                render_tape(w, h, (float)p->head.index);
+                render_head(w, h, sinstep(p->t));
+
+                if (p->t >= 1.0) {
+                    p->head.state = arena_strdup(&p->tape_strings, action.as.sweetch);
+
                     p->ip += 1;
                     p->t = 0;
                 }
@@ -344,7 +430,7 @@ void plug_update(float dt, float w, float h, bool _rendering) {
                 }
 
                 render_tape(w, h, (float)p->head.index);
-                render_head(w, h, 1.0f);
+                render_head(w, h, 0.0f);
 
                 if (p->t >= 1.0) {
                     assert(0 <= p->head.index);
